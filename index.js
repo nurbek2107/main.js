002bdd3085706a44b7cd7d50ba05cf28ac428325
nurbek2107 <abduaxatov002@gmail.com>
2024-03-30 22:53:26 +0500

    feat:mainjs

---
+++ index.js
@@ -0,0 +1,16 @@
+for (let i = 2; i <= 100; i++) {
+    let a = true;
+    if (i === 2) {
+        console.log(i);
+    } else if (i > 2) {
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                a = false;
+                break;
+            }
+        }
+        if (a) {
+            console.log(i);
+        }
+    }
+}
